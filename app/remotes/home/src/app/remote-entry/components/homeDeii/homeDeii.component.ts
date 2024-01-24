c552ee92d4e789a43a64eadeefce347cf5db2420
DESKTOP-UGHDMMU\Kike <mehernandez@soaint.com>
2024-01-24 06:13:36 -0500

    FIX: se realizan ajustes adicionales

--- app/remotes/home/src/app/remote-entry/components/homeDeii/homeDeii.component.ts
+++ app/remotes/home/src/app/remote-entry/components/homeDeii/homeDeii.component.ts
@@ -5,11 +5,20 @@ import {FooterComponent} from "@microfrontend-dei/footer";
 import {HeaderComponent} from "@microfrontend-dei/header";
 import {SidebarComponent} from "@microfrontend-dei/sidebar";
 import {SubheaderComponent} from "@microfrontend-dei/subheader";
+import {RevisionDeteccionesComponent} from "../../../../../../gestionar-detecciones/src/app/remote-entry/components/revision-detecciones/revision-detecciones.component";
 
 @Component({
   selector: 'microfrontend-dei-home-deii',
   standalone: true,
-  imports: [CommonModule, RouterModule, FooterComponent, HeaderComponent,SidebarComponent, SubheaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    SubheaderComponent,
+   // RevisionDeteccionesComponent //SE PONE EL COMPONENTE DE REVISIONES PROVISIONALMENTE, SE TRABAJA EN UNA SOLUCION PARA LAS RUTAS HIJAS
+  ],
   templateUrl: './homeDeii.component.html',
   styleUrl: './homeDeii.component.sass',
 })
